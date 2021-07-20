741a16c630dcbdb72e9bec3b502d4aca5a5ee65c
Ukrainets Aleksandr <ukr-sasha@breeze-soft.com>
2021-07-20 09:26:27 +0300

    Add project files.

---
+++ PDD_Ukraine/PDD_Ukraine/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PDD_Ukraine.Models;
+using PDD_Ukraine.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDD_Ukraine.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
